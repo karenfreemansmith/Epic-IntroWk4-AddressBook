f8de132f527fa688947dc7d5214a83ae59c0dda8
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-08-20 19:51:41 -0700

    complete working version of homework

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+//backend logic
+function Contact(first, last) {
+  this.firstName=first;
+  this.lastName=last;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+//frontend logic
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var first = $("input#new-first-name").val();
+    var last = $("input#new-last-name").val();
+    var newContact=new Contact(first, last);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName +"</span></li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
+  });
+
+
+});
